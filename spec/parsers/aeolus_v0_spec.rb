d9cf08a6237edb2f30a5ff5d87559f9768178b78
Tomas Sedovic <tomas@sedovic.cz>
2012-12-11 11:30:33 +0100

    Aeolus: add more thorough tests
    Signed-off-by: Tomas Sedovic <tomas@sedovic.cz>

--- spec/parsers/aeolus_v0_spec.rb
+++ spec/parsers/aeolus_v0_spec.rb
@@ -8,7 +8,32 @@ describe 'Aeolus v.0 parser' do
 
   it "must successfully parse the wordpress deployable" do
     deployable_path = "#{SAMPLES_PATH}/aeolus_v0/wordpress.xml"
-    CBF.parse(:aeolus_v0, open(deployable_path))
+    result = CBF.parse(:aeolus_v0, open(deployable_path))
+    result[:name].must_equal "Wordpress Multi-Instance Deployable"
+    result[:description].must_equal "This is an example of a multi deployment that deploys wordpress across an apache and mysql instance"
+
+    result[:resources].count.must_equal 2
+    result[:resources][0][:name].must_equal "webserver"
+    result[:resources][1][:name].must_equal "database"
+
+    result[:services].count.must_equal 2
+    result[:services][0][:name].must_equal "http"
+    result[:services][1][:name].must_equal "mysql"
+
+    http_params = result[:parameters].select do
+      |p| p[:resource] == "webserver" && p[:service] == "http"
+    end
+    http_params.count.must_equal 6
+
+    mysql_params = result[:parameters].select do
+      |p| p[:resource] == "database" && p[:service] == "mysql"
+    end
+    mysql_params.count.must_equal 5
+
+    result[:files].count.must_equal 3
+    result[:files].select { |f| f[:executable] }.count.must_equal 2
+
+    result[:outputs].count.must_equal 5
   end
 
   it "must successfully parse the drupal deployable" do

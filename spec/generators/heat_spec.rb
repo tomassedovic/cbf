c6694ddf7da67d0e146fdce17e183d59b3053d69
Tomas Sedovic <tomas@sedovic.cz>
2013-02-26 16:54:49 +0100

    Heat: tests files are generated properly
    Signed-off-by: Tomas Sedovic <tomas@sedovic.cz>

--- spec/generators/heat_spec.rb
+++ spec/generators/heat_spec.rb
@@ -66,4 +66,45 @@ describe 'Heat generator' do
     properties.must_include 'KeyName'
     properties['KeyName'].must_equal 'my-key'
   end
+
+  it "must ensure all files have filenames" do
+    source = {
+      :description => 'sample template',
+      :parameters => [],
+      :resources => [{
+        :name => 'my instance',
+        :type => :instance,
+        :image => 'test-image-id',
+        :hardware_profile => 'test-hwp',
+        :key_name => 'my-key'
+      }],
+      :files => [
+        {
+          :executable => true,
+          :resource => 'my instance',
+          :location => '/tmp/files',
+          :mode => '000755',
+          :environment => [],
+        },
+        {
+          :name => 'setup.mysql',
+          :executable => false,
+          :resource => 'my instance',
+          :location => '/tmp/files',
+          :mode => '000755',
+          :environment => [],
+        },
+      ],
+    }
+
+    t = JSON.parse(CBF.generate(:heat, source))
+    inst = t['Resources']['my instance']
+
+    # When we specify an executable file, UserData must be present
+    inst['Properties'].must_include 'UserData'
+
+    files = inst['Metadata']['AWS::CloudFormation::Init']['config']['files']
+    files.must_include '/tmp/files/my-instance-file'
+    files.must_include '/tmp/files/setup.mysql'
+  end
 end
